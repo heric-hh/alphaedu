a0a5bfcb79c16ae2cf8ca4f9732e62dce6377c59
Star7-jpg <marcosconde612@gmail.com>
2025-05-20 13:29:43 -0600

    accidental fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import About from "./components/About";
+
 export default function App() {
   return (
-    <div className="border h-1 text font-extrabold">AlphaEdu Site</div>
+    <>
+      <div className="border h-1 text font-extrabold">AlphaEdu Site</div>
+      <About />
+    </>
+
   )
 }
